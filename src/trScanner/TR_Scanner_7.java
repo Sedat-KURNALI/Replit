ef34d88d8578d1332cd6f9eabaa504d8fd64f8a0
Sedat-KURNALI <sedatkurnali@gmail.com>
2022-09-03 17:09:40 +0300

    2022.09.03_v01

---
+++ src/trScanner/TR_Scanner_7.java
@@ -0,0 +1,15 @@
+package trScanner;
+
+public class TR_Scanner_7 {
+    public static void main(String[] args) {
+        /*
+            Ilk satira Hello ikinci satira World! yazdiran praogramin java kodunu yaziniz.
+            Ornek Cikti:
+                Hello
+                World!
+        */
+
+
+
+    }
+}
---
+++ src/trScanner/TR_Scanner_9.java
@@ -0,0 +1,15 @@
+package trScanner;
+
+public class TR_Scanner_9 {
+    public static void main(String[] args) {
+        /*
+            Ilk satira Hello ikinci satira World! yazdiran praogramin java kodunu yaziniz.
+            Ornek Cikti:
+                Hello
+                World!
+        */
+
+
+
+    }
+}
